76c2b806240f7c3dd59a604a42d83b3bf4347051
Beny <b.ntambwe97@gmail.com>
2021-01-13 17:06:52 +0100

    almost end

---
+++ public/js/main.js
@@ -0,0 +1,51 @@
+// EXO 1
+// let nbr1 = (a) => {
+//     return [...a].reverse().join("");
+// }
+// console.log(nbr1("851"));
+// EXO 2
+// let nbr2 = (a,b) => {
+//     switch (key) {
+//         case '2':
+            
+//             break;
+    
+//         default:
+//             break;
+//     }
+// }
+
+// EXO 3
+
+// function CheckPassword(inputtxt) 
+// { 
+// var passw=  /^[A-Za-z]\w{7,14}$/;
+// if(inputtxt.value.match(passw)) 
+// { 
+// alert('Correct, try another...')
+// return true;
+// }
+// else
+// { 
+// alert('Wrong...!')
+// return false;
+// }
+// }
+// let quest = prompt("Entrez votre mot de passe")
+// let user = (a) => {
+//     if ((${a} !== 'mdp')) {
+//         prompt('Entrez votre mot de passe')
+//         return false
+// }
+// return true;
+// }
+// let quest = prompt("Entrez votre mot de passe")
+// let user = (a) => {
+//     var password = a ('password').value;
+//     if ( password == "mdp")
+//     alert('Correct!')
+//     else
+//     alert('Wrong Password')
+//     }
+// console.log();    
+// EXO 4
